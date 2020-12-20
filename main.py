ae238ac4c2556f13a614df5c9159b11ac2de271b
Yas Elh <yassineelhimr@gmail.com>
2020-12-20 22:50:15 +0000

    added structured classes

--- main.py
+++ main.py
@@ -3,49 +3,65 @@ import cv2
 import torch
 import numpy as np
 
-def vconcat_resize_min(im_list, interpolation=cv2.INTER_CUBIC):
-    w_min = min(im.shape[1] for im in im_list)
-    im_list_resize = [cv2.resize(im, (w_min, int(im.shape[0] * w_min / im.shape[1])), interpolation=interpolation)
-                      for im in im_list]
-    return cv2.vconcat(im_list_resize)
+class recognizer:
+    def __init__(self):
+        pass
 
-def ocr_core(filename):
-    """
-    This function will handle the core OCR processing of images.
-    """
-    image = cv2.imread(filename, 0)
-    #gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    def predict(self, image):
+        """
+        This function will handle the core OCR processing of images.
+        """
+        #gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
-    # converting image to binary
-    #ret, thresh = cv2.threshold(gray_image, 127, 255, 0)
-    #thresh = cv2.bitwise_not(thresh)
-    digits = []
+        # converting image to binary
+        #ret, thresh = cv2.threshold(gray_image, 127, 255, 0)
+        #thresh = cv2.bitwise_not(thresh)
+        digits = []
     
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    model = ln.LetNet5().to(device)
-    model.load()
+        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        model = ln.LetNet5().to(device)
+        model.load()
  
-    #Todo: remove this -> find learned roi 
-    #h,w = 55,43 #scut difficult
-    h,w = 100,78 #scut easy
-    #h,w = 46,42 #black
-    for i in range(0,5):
-        offset = i * w
-        roi = image[0:100, offset:offset+w]
+        #Todo: remove this -> find learned roi 
+        #h,w = 55,43 #scut difficult
+        h,w = 100,78 #scut easy
+        #h,w = 46,42 #black
+        for i in range(0,5):
+            offset = i * w
+            roi = image[0:100, offset:offset+w]
 
-        # predict
-        pred = model.predict(roi)
-        digits.append(pred)
+            # predict
+            pred = model.predict(roi)
+            digits.append(pred)
 
-    print(digits)
+        return digits
 
-pred_list = [
-    'images/test/cropped_1.png',
-    'images/test/cropped_2.png'
-]
 
-imgs = []
-for pred in pred_list:
-    imgs.append(cv2.imread(pred))
-    ocr_core(pred)
-cv2.imwrite("res.png", vconcat_resize_min([img for img in imgs]))
+
+
+class util:
+    def vconcat_resize_min(im_list, interpolation=cv2.INTER_CUBIC):
+        w_min = min(im.shape[1] for im in im_list)
+        im_list_resize = [cv2.resize(im, (w_min, int(im.shape[0] * w_min / im.shape[1])), interpolation=interpolation)
+                      for im in im_list]
+        return cv2.vconcat(im_list_resize)
+
+
+if __name__ == "__main__":
+    
+    #list of image to perform predition on:
+    pred_list = [
+        'images/test/cropped_1.png',
+        'images/test/cropped_2.png'
+    ]
+
+    #model used to predict:
+    model = recognizer()
+
+    imgs = []
+    for pred in pred_list:
+        im = cv2.imread(pred, 0)
+        imgs.append(im)
+        predictions = model.predict(im)
+        print(predictions)
+    cv2.imwrite("res.png", util.vconcat_resize_min([img for img in imgs]))
